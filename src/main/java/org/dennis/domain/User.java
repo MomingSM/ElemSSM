ceefd526632aea38afa5cc8e25e4f0fae584e13a
Dennisccc <137410099@qq.com>
2020-10-22 11:56:57 +0800

    2nd update test development environment

---
+++ src/main/java/org/dennis/domain/User.java
@@ -0,0 +1,47 @@
+package org.dennis.domain;
+
+import lombok.Data;
+
+import java.util.Date;
+@Data
+public class User {
+    private Integer uid;
+
+    private String username;
+
+    private String password;
+
+    private Date addTime;
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(Date addTime) {
+        this.addTime = addTime;
+    }
+}
